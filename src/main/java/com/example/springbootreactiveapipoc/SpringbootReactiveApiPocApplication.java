9c13f2396e76ab358fdc1322e65a528f460a9f5b
Nazri <649000@users.noreply.github.com>
2021-11-21 16:26:14 +0800

    Initial Commit

---
+++ src/main/java/com/example/springbootreactiveapipoc/SpringbootReactiveApiPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootreactiveapipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootReactiveApiPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootReactiveApiPocApplication.class, args);
+	}
+
+}
